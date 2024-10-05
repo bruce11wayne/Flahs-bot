6d6421432d71fd4588bca1f28eea8d39bd215b31
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-10-06 00:23:02 +0300

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -14,7 +14,7 @@ stiker = await addExif(img, packname || '', author || '')
 console.error(e)
 if (Buffer.isBuffer(e)) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `𝐇𝐚𝐫𝐥𝐞𝐲 𝐥𝐢𝐠𝐡𝐭`, mediaType: 2, sourceUrl: canal1, thumbnail: imagen1}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `𝐔𝐧𝐨𝐡𝐚𝐧𝐚`, mediaType: 2, sourceUrl: canal1, thumbnail: imagen1}}}, { quoted: m })
 else throw '╰⊱❗️⊱ *الامر واقف يابن الفقرية 🗿* ⊱❗️⊱╮'
 }}
 handler.help = ['wm <packname>|<author>']
