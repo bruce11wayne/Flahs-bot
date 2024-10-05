5f69465b9e89b2ce7f6104f03fc7c9c841388977
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-10-06 00:34:51 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
-await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-    conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
+    let message = '*مــنــور يـا قــلــب اونـوهـانـا الاوامـر اكـتـب(.اوامر) 🧸*';
+await conn.sendMessage(m.chat, { react: { text: '🧭', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/QkiAz.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^بوت$/i;
