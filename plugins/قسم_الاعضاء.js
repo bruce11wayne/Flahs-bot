796e7bf917a8300e94f9287b3ef8a622842c23db
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-10-06 17:02:13 +0300

    Update قسم_الاعضاء.js

--- plugins/قسم_الاعضاء.js
+++ plugins/قسم_الاعضاء.js
@@ -68,7 +68,7 @@ let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
         }
 
         function _0x42ca() {
-            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', 'Respect-team', '406070MLYaeB', 'https://chat.whatsapp.com/Ilr00RrPBu95bxkxQ5rZbs', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
+            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', 'reception', '406070MLYaeB', 'https://chat.whatsapp.com/Ilr00RrPBu95bxkxQ5rZbs', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
             _0x42ca = function () {
                 return _0xe1e20e;
             };
