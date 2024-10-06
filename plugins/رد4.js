a198c9d2e53297cc6a7a6eac88f10d03f08c6239
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-10-06 22:58:49 +0300

    Create رد4.js

---
+++ plugins/رد4.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = '*مـوجـودة لا تـخـاف اكـتـب(.اوامر)*';
+await conn.sendMessage(m.chat, { react: { text: '😶‍🌫️', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/aPkZ.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^تست$/i;
+handler.command = new RegExp;
+
+export default handler;
