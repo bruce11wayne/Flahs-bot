cb690f98c2e2f1814f0280f1c55486f2c46b8b7d
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-10-06 17:16:25 +0300

    Update قسم_الاعضاء.js

--- plugins/قسم_الاعضاء.js
+++ plugins/قسم_الاعضاء.js
@@ -7,7 +7,7 @@ import { promises } from 'fs'
 import { join } from 'path'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
 try {
-let vn = './Menu2.jpg'
+let vn = 'Menu2.jpg'
 let pp = imagen4
 let img = await(await fetch('https://qu.ax/ORrM.jpg')).buffer()
 let d = new Date(new Date + 3600000)
