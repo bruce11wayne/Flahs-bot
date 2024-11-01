28560b75f164a51e25abca4c9e8cac0b888dc654
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-11-02 00:52:43 +0300

    Update قسم_الاعضاء.js

--- plugins/قسم_الاعضاء.js
+++ plugins/قسم_الاعضاء.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    let videoUrl = 'https://qu.ax/ORrM.jpg';
+    let videoUrl = 'https://telegra.ph/file/772acd48a378af8d390af.mp4';
     let vn = '.Menu.png';
     const user = global.db.data.users[m.sender];
     const {money, joincount} = global.db.data.users[m.sender];
