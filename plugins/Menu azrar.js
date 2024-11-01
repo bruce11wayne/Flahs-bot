7c285063d0262101b6a9cb1db980a14dc825b0b8
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-11-02 00:30:01 +0300

    Update Menu azrar.js

--- plugins/Menu azrar.js
+++ plugins/Menu azrar.js
@@ -1,124 +1,135 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
-const { levelling } = '../lib/levelling.js'
-import PhoneNumber from 'awesome-phonenumber'
-import { promises } from 'fs'
-import { join } from 'path'
-let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
-try {
-let vn = '/Menu2.jpg'
-let pp = imagen4
-let img = await(await fetch('https://qu.ax/ORrM.jpg')).buffer()
-let d = new Date(new Date + 3600000)
-let locale = 'ar'
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime)
-let user = global.db.data.users[m.sender]
-let { money, joincount } = global.db.data.users[m.sender]
-let { exp, limit, level, role } = global.db.data.users[m.sender]
-let { min, xp, max } = xpRange(level, global.multiplier)
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-let more = String.fromCharCode(8206)
-let readMore = more.repeat(850)   
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
-> 𝐔𝐧𝐨𝐡𝐚𝐧𝐚﹝🧭﹞𝐁𝐎𝐓
-*⎔⋅ ╼╃ ⊰ •﹝⚜️﹞• ⊱ ╄╾ ⋅⎔*
-> 〔قـسـم الـمـشـرفـيـن┊ ˼‏ 🧑‍💻˹ 〕
-*⎔⋅ ╼╃ ⊰ •﹝⚜️﹞• ⊱ ╄╾ ⋅⎔*
-           *اونوهانا بــ🤖ــوت*
-*⎔⋅ ╼╃ ⊰ •﹝⚜️﹞• ⊱ ╄╾ ⋅⎔*
-> 🧑‍💻.طرد
-> 🧑‍💻.ترقية
-> 🧑‍💻.اعفاء
-> 🧑‍💻.تغير-الاسم
-> 🧑‍💻.حذف
-> 🧑‍💻.منشن
-> 🧑‍💻.مخفي
-> 🧑‍💻.دعوه
-> 🧑‍💻.تغيير_اللينك
-> 🧑‍💻.جروب قفل او فتح
-> 🧑‍💻.لينك
-> 🧑‍💻.تسجيل
-> 🧑‍💻.حذف_لقب
-> 🧑‍💻.لقب
-> 🧑‍💻.لقبه
-*⎔⋅ ╼╃ ⊰ •﹝⚜️﹞• ⊱ ╄╾ ⋅⎔*
-*~⌬ تــ✍️︎ــوقــيــع ↡~*🖋
-*𝐔𝐧𝐨𝐡𝐚𝐧𝐚﹝🧭﹞𝐁𝐎𝐓*
-*⎔⋅ ╼╃ ⊰ •﹝⚜️﹞• ⊱ ╄╾ ⋅⎔*`.trim()
-   const _0x3c7cab = _0x5a1d;
-
-        function _0x5a1d(_0x1b92fa, _0x42ca62) {
-            const _0x5a1d39 = _0x42ca();
-            _0x5a1d = function (_0x28d0f5, _0x342e36) {
-                _0x28d0f5 = _0x28d0f5 - 0x0;
-                let _0x4abc25 = _0x5a1d39[_0x28d0f5];
-                return _0x4abc25;
-            };
-            return _0x5a1d(_0x1b92fa, _0x42ca62);
-        }
-
-        function _0x42ca() {
-            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', 'reception', '406070MLYaeB', 'https://chat.whatsapp.com/Ilr00RrPBu95bxkxQ5rZbs', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
-            _0x42ca = function () {
-                return _0xe1e20e;
-            };
-            return _0x42ca();
-        }(function (_0x2979d9, _0x233f8f) {
-            const _0x32315e = _0x5a1d;
-            const _0x3b1890 = _0x2979d9();
-            while (!![]) {
-                try {
-                    const _0x2cc4d6 = parseInt(_0x32315e(0x5)) / 0x1 + parseInt(_0x32315e(0x1)) / 0x2 + -parseInt(_0x32315e(0xa)) / 0x3 * (parseInt(_0x32315e(0x3)) / 0x4) + parseInt(_0x32315e(0x7)) / 0x5 + -parseInt(_0x32315e(0xf)) / 0x6 * (parseInt(_0x32315e(0xd)) / 0x7) + parseInt(_0x32315e(0x4)) / 0x8 * (-parseInt(_0x32315e(0x9)) / 0x9) + -parseInt(_0x32315e(0xb)) / 0xa * (-parseInt(_0x32315e(0xe)) / 0xb);
-                    if (_0x2cc4d6 === _0x233f8f) {
-                        break;
-                    } else {
-                        _0x3b1890['push'](_0x3b1890['shift']());
-                    }
-                } catch (_0x3284ad) {
-                    _0x3b1890['push'](_0x3b1890['shift']());
-                }
-            }
-        }(_0x42ca, 0x772cd));
-        let buttonMessage = {
-            'image': pp,
-            'caption': str[_0x3c7cab(0xc)](),
-            'mentions': [m[_0x3c7cab(0x8)]],
-            'footer': '' + wm,
-            'headerType': 0x4,
-            'contextInfo': {
-                'mentionedJid': [m['sender']],
-                'externalAdReply': {
-                    'showAdAttribution': !![],
-                    'mediaType': _0x3c7cab(0x6),
-                    'mediaUrl': null,
-                    'title': _0x3c7cab(0x0),
-                    'body': null,
-                    'thumbnail': img,
-                    'sourceUrl': _0x3c7cab(0x2)
-                }
-            }
-        };
-        conn.sendMessage(m.chat, buttonMessage, {
-            quoted: m
-        })
-
-    } catch {
-        conn.reply(m.chat, '[❗خطاء❗]', m)
-    }
+    let m = Math.floor(ms % 3600000 / 60000);
+    let s = Math.floor(ms % 60000 / 1000);
+    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
 }
-handler.command = /^(ق 10|قسم المشرفين|ق10|ق_10)$/i;
-handler.exp = 20
-handler.fail = null
-export default handler
-
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-                      }
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = pkg;
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    let d = new Date(new Date + 3600000);
+    let locale = 'ar';
+    let week = d.toLocaleDateString(locale, { weekday: 'long' });
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' });
+    let _uptime = process.uptime() * 1000;
+    let uptime = clockString(_uptime);
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender)
+    let { money, joincount } = global.db.data.users[m.sender];
+    let { exp, limit, level, role } = global.db.data.users[m.sender];
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
+    let more = String.fromCharCode(8206);
+    let readMore = more.repeat(850);
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+  await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } })
+  const zack = 'https://qu.ax/ORrM.jpg'
+  const mentionId = m.key.participant || m.key.remoteJid;
+ 
+conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { header: { title: `harley`}, body: { text: `˼🧭˹↜ مـࢪحـبـا بـك/ي @${mentionId.split('@')[0]}
+> ˼🪪˹↜ مــعــلــومــاتــك ↶
+╮───────────────────⟢ـ
+┆⚡↜ بـريـمـيـوم↞⌊ ${user.premiumTime > 0 ? 'مــمـ🔱ـيز' : (isPrems ? 'مــمـ🔱ـيز' : 'عــ🍁ــادي') || ''} ⌉
+┆⚜️↜ مـــســـتواك↞⌊ ${level} ⌉
+┆💫↜ رتـبـتـك↞⌊ ${role} ⌉
+┆🧰↜ الـخـبـرة↞⌊ ${exp} ⌉
+┆💎↜ الـمـاس↞⌊ ${limit} ⌉
+╯───────────────────⟢ـ
+> ˼🤖˹↜ الــبــوت↶
+╮───────────────────⟢ـ
+┆⚙️ ↜اسـم الـبـوت↶﹝𝐔𝐧𝐨𝐡𝐚𝐧𝐚﹞
+┆🪄 ↜الـمـطـور ↶﹝BRUCE WAYNE﹞
+┆📌 ↜الـتـشـغـيـل ↶﹝${uptime}﹞
+┆🔖 ↜الــمــســتـخـدمـيـن ↶﹝${rtotalreg}﹞
+╯───────────────────⟢ـ
+> 𝐔𝐧𝐨𝐡𝐚𝐧𝐚﹝🧭﹞𝐁𝐎𝐓`,subtitle: "BRUCE WAYNE",},header: { hasMediaAttachment: true,...(await prepareWAMessageMedia({ image : { url: zack } }, { upload: conn.waUploadToServer }, {quoted: m}))},
+                    contextInfo: {
+                        mentionedJid: [m.sender],
+                        isForwarded: false,
+                    },nativeFlowMessage: { buttons: [
+                                                          {
+                                name: 'single_select',
+                                buttonParamsJson: JSON.stringify({
+                                    title: '⌈🛡╎الــقــوائـــم╎🛡⌋',
+                                    sections: [
+                                        {
+                                            title: 'مــرحـ🛡ـبــا بــك فـي مــ☑هــام اونوهانا بـ🤖ـوت',
+                                            highlight_label: 'خذ راحتك 🫦',
+                                            rows: [
+                                                {
+                                                    header: 'الــقـ👑ـســم الـاول',
+                                                    title: 'استدعاء_قسم_الاعضاء #الاعضاء',
+                                                    description: '',
+                                                    id: '.ق1'
+                                                },
+                                                {
+                                                    header: 'الــقـ👨🏻‍💻ـســم الــثــانــي',
+                                                    title: 'استدعاء_قسم_المشرفين #المشرفين',
+                                                    description: '',
+                                                    id: '.ق10'
+                                                },
+                                                {
+                                                    header: 'الــقـ🕋ـســم الــثــالــث',
+                                                    title: 'استدعاء_قسم_الدين #الدين',
+                                                    description: '',
+                                                    id: '.ق2'
+                                                },
+                                                {
+                                                    header: 'الــقـ👑ـســم الــرابــع',
+                                                    title: 'استدعاء_قسم_المطور #المطور',
+                                                    description: '',
+                                                    id: '.ق3'
+                                                },
+                                                {
+                                                    header: 'الــقـ🛡ـســم الــخــامــس',
+                                                    title: 'استدعاء_قسم_التنزيلات #التنزيلات',
+                                                    description: '',
+                                                    id: '.ق4'
+                                                },
+                                                {
+                                                    header: 'الــقـ🕹ـســم الــســادس',
+                                                    title: 'استدعاء_قسم_الالعاب #الالعاب',
+                                                    description: '',
+                                                    id: '.ق5'
+                                                },
+                                                {
+                                                    header: 'الــقـ🌀ـســم الــســابــع',
+                                                    title: 'استدعاء_قسم_التحويلات #التحويلات',
+                                                    description: '',
+                                                    id: '.ق6'
+                                                },
+                                                {
+                                                    header: 'الــقـ🤖ـســم الــتــاســع',
+                                                    title: 'استدعاء_قسم_الذكاء #الذكاء',
+                                                    description: '',
+                                                    id: '.ق7'
+                                                },
+                                                {
+                                                    header: 'الــقـ🚨ـســم الــعــاشــر',
+                                                    title: 'استدعاء_قسم_الدعم #الدعم',
+                                                    description: '',
+                                                    id: '.ق8'
+                                                },
+                                                {
+                                                    header: 'الــقـ🔍ـســم �لــحــاديــة عــشــر',
+                                                    title: 'استدعاء_قسم_ابحث #البحث',
+                                                    description: '',
+                                                    id: '.ق11'
+                                               }
+                                            ]
+                                        }
+                                    ]
+                                }),
+                  messageParamsJson: ''
+                     },
+                     {
+               name: "cta_url",
+               buttonParamsJson: '{"display_text":"⌈📲╎قـنـاة الــبــوت╎📲⌋","url":"https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g","merchant_url":"https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g"}'
+                            }
+                        ]
+        function _0x42ca(
+                  }
+    }, {});
+                                         }
+                                                                               handler.help = ['info'];
+handler.tags = ['main'];
+handler.command = ['menu', 'مهام', 'اوامر','الاوامر','قائمة','القائمة']
+                                                                               export default handler;
