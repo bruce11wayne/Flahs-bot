0e0ae8c3c8f942f7a9c92fbdd1f7d7b90616527c
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-11-08 19:04:19 +0300

    Update المالك.js

--- plugins/المالك.js
+++ plugins/المالك.js
@@ -1,4 +1,5 @@
 function handler(m) {
+      conn.sendFile(m.chat, 'https://qu.ax/dnRWN.mp4', 'menu.mp3', message, m);
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
