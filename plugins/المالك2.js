78e454949c7d8969896720b395a56b427f22e6a8
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-11-08 18:47:19 +0300

    Create المالك2.js

---
+++ plugins/المالك2.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = '*ارقـام مـطـوريـنـي لا تـزعـجـوهـم 🍒*';
+await conn.sendMessage(m.chat, { react: { text: '🧭', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/dnRWN.mp4', 'menu.mp3', message, m);
+};
+
+handler.customPrefix = /^المطور$/i;
+handler.command = new RegExp;
+
+export default handler;
