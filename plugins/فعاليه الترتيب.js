ea704d658275370e70bd8e6762b44e4f9de1db0b
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-11-10 16:43:34 +0300

    Update فعاليه الترتيب.js

--- plugins/فعاليه الترتيب.js
+++ plugins/فعاليه الترتيب.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 ❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
 ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『𝐁𝐎𝐀𝐘𝐊𝐀﹝❄️﹞𝐁𝐎𝐓』
+> 『🧭𝚄𝚗𝚘𝚑𝚊𝚗𝚊–𝙱𝙾𝚃🧭』
 `.trim();
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
