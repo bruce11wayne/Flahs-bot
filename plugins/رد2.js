876fbca8df1a6856f754537262d248f4c199235e
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-12-18 19:43:25 +0300

    Add files via upload

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب اونـوهـانـا الاوامـر اكـتـب(.اوامر) 🧸*';
-await conn.sendMessage(m.chat, { react: { text: '🧭', key: m.key } })
-    conn.sendFile(m.chat, 'https://qu.ax/QkiAz.jpg', 'image.jpg', message, m);
+    let message = '*مـرحـبـا انـا غـاتـس لـي طـلـب الاوامـر اكـتـب (.اوامر) 🕸️*';
+await conn.sendMessage(m.chat, { react: { text: '🕸️', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/mjkwd.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^بوت$/i;
